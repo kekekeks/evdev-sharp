2dda60905115f795fbce5e350e39d726f6d4ae75
Nikita Tsukanov <keks9n@gmail.com>
2019-07-19 18:27:03 +0300

    Initial commit with device info

---
+++ src/EvDevSharp/Structs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using __s32 = System.Int32;
+using __u16 = System.UInt16;
+using __u32 = System.UInt32;
+
+namespace EvDevSharp
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct input_event
+    {
+        private IntPtr crap1, crap2;
+        public ushort type, code;
+        public int value;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    unsafe struct fd_set
+    {
+        public int count;
+        public fixed int fds [256];
+    }
+
+    
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct input_absinfo
+    {
+        public __s32 value;
+        public __s32 minimum;
+        public __s32 maximum;
+        public __s32 fuzz;
+        public __s32 flat;
+        public __s32 resolution;
+
+    }
+}
---
+++ src/EvTest/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using EvDevSharp;
+
+namespace EvTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var devices = EvDevDeviceInfo.EnumerateDevices()
+                .Select(d =>
+                {
+                    var match = Regex.Match(d.DevicePath, @"\/dev\/input\/event([0-9]+)");
+                    if (match.Success)
+                        return (int.Parse(match.Groups[1].Value), d);
+                    else
+                        return (int.MaxValue, d);
+                }).OrderBy(x => x.Item1).ThenBy(x => x.Item2).Select(x => x.Item2).ToList();
+            
+
+            void Tab(int count, string s)
+            {
+                for (var c = 0; c < count; c++)
+                    Console.Write("    ");
+                Console.WriteLine(s);
+            }
+            foreach (var dev in devices)
+            {
+                Console.WriteLine("Device " + dev.DevicePath);
+                Tab(1, $"Id: {dev.Id}");
+                Tab(1, $"Name: {dev.Name}");
+                Tab(1, $"Guessed type: {dev.GuessedDeviceType}");
+                Tab(1, "Properties: "+string.Join(",", dev.Properties));
+                if (dev.RelativeAxises != null)
+                    Tab(1, "Relative axises: " + string.Join(",", dev.RelativeAxises.OrderBy(x => x)));
+                if (dev.AbsoluteAxises != null)
+                {
+                    Tab(1, "Absolute axises: ");
+                    foreach (var axis in dev.AbsoluteAxises.OrderBy(x => x.Key))
+                    {
+
+                        Tab(2, "\t" + axis.Key);
+                        Tab(3, $"Value: {axis.Value.Value}");
+                        Tab(3, $"Min: {axis.Value.Min}");
+                        Tab(3, $"Max: {axis.Value.Max}");
+                        if (axis.Value.Resolution != 0)
+                            Tab(3, $"Resolution: {axis.Value.Resolution}");
+                        if (axis.Value.Flat != 0)
+                            Tab(3, $"Flat: {axis.Value.Flat}");
+                        if (axis.Value.Fuzz != 0)
+                            Tab(3, $"Fuzz: {axis.Value.Fuzz}");
+
+                    }
+                }
+
+                if (dev.Keys != null)
+                {
+                    Tab(1, "Keys:");
+                    foreach (var k in dev.Keys)
+                        Tab(2, k.ToString());
+                }
+            }
+            
+        }
+    }
+}
